2fdcbbb8bb762b130dfe95ee3bfe4d98a37c6344
Alicia Santiago <albrown07@mac.com>
2019-02-07 16:31:56 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -14,8 +14,8 @@ def greeting(greeting, name)
  puts "#{greeting} #{name}"
 end
 
-def return_a_value("Nice")
-  puts return_a_value
+def return_a_value
+  return_a_value
 end
 
 def last_evaluated_value
